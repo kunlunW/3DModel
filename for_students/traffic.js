0bd465803e34fb6cd50981e4adb8b271e62c5ffc
kunlunW <52982585+kunlunW@users.noreply.github.com>
2020-05-01 22:58:10 -0500

    Update traffic.js

--- for_students/traffic.js
+++ for_students/traffic.js
@@ -111,3 +111,84 @@ function shift(grobj,x,z,s=1) {
 }
 
 
+export class HelicopterObject extends GrObject{
+    constructor(id){
+        let group = new T.Group();
+        super("Helicopter2",group);
+        this.cop = group;
+        this.id = id;
+        let backBlade = new T.MeshStandardMaterial({color:"black"});
+        let head = new T.SphereBufferGeometry(.6,20,20);
+        let mesh = new T.Mesh(head, new T.MeshStandardMaterial({color:"white", metalness: 0.8}));
+        this.cop.add(mesh);
+        this.t = new T.CylinderBufferGeometry(.02,.4,1.2,40)
+        let tailMesh = new T.Mesh(this.t, new T.MeshStandardMaterial({color:"white",metalness:0.8 }));
+        tailMesh.rotateZ(Math.PI/2);
+        tailMesh.position.x = tailMesh.position.x-.6;
+        this.cop.add(tailMesh);
+        let rb = new T.CylinderBufferGeometry(.01,.01,.2,15)
+        let rm = new T.Mesh(rb,backBlade);
+        rm.position.y = rm.position.y+.45;
+        this.cop.add(rm);
+        let tb = new T.BoxBufferGeometry(.01,.3,.07);
+        let tbm = new T.Mesh(tb, backBlade);
+        tbm.position.x = tailMesh.position.x-.58;
+        tbm.position.z = tailMesh.position.z-.025;
+        tbm.rotateY(Math.PI/2);
+        this.cop.add(tbm);
+        let te = new T.BoxBufferGeometry(.01,.3,.07);
+        this.tem = new T.Mesh(te, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+        this.tem.position.x = tbm.position.x;
+        this.tem.position.z = tbm.position.z-.015;
+        this.tem.rotateY(Math.PI/2);
+        this.cop.add(this.tem);
+
+        this.rotor2 = new T.BoxBufferGeometry(.008,1.5,.07);
+        this.rotor = new T.BoxBufferGeometry(.008,1.5,.07);
+    
+        this.rotor3 = new T.BoxBufferGeometry(.008,1.5,.07);
+        this.rotor4 = new T.BoxBufferGeometry(.008,1.5,.07);
+
+        this.base = new T.BoxBufferGeometry(0.15,0.15,0.15);
+
+        let rotorMesh = new T.Mesh(this.rotor, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+        let roterMesh2 = new T.Mesh(this.rotor2, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+        let roterMesh3 = new T.Mesh(this.rotor2, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+
+        let roterMesh4 = new T.Mesh(this.rotor2, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+
+        let baseMesh = new T.Mesh(this.base, new T.MeshStandardMaterial({color:"black", metalness:0.8}));
+        roterMesh2.position.y = tailMesh.position.y+.59;
+        rotorMesh.position.y = tailMesh.position.y+.59;
+        baseMesh.position.y = tailMesh.position.y+.59;
+        roterMesh3.position.y = tailMesh.position.y+.59;
+        roterMesh4.position.y = tailMesh.position.y+.59;
+
+        rotorMesh.rotateZ(Math.PI/2);
+    
+
+        roterMesh2.rotateZ(-Math.PI/2)
+    
+        this.cop.add(rotorMesh, roterMesh2, baseMesh);
+
+        this.cop.position.y = 2
+
+        this.ridePoint = new T.Object3D();
+        this.ridePoint.translateY(2);
+        this.ridePoint.rotateY(Math.PI / 2);
+        this.objects[0].add(this.ridePoint);
+        this.rideable = this.ridePoint;
+    }
+
+    tick(delta, timeofday){
+        let theta = performance.now() / 800;
+        let x2 = this.id * -28 * Math.cos(theta);
+        let z2 = this.id * -28 * Math.sin(theta);
+        this.cop.position.x = x2;
+        this.cop.position.z = z2;
+        this.cop.lookAt(2,2,2);
+        this.tem.rotateX(3);
+        this.cop.position.y = this.id * Math.sin(theta) * 3.5 + 20;
+        this.rotor.rotateX(1);
+    };
+}
